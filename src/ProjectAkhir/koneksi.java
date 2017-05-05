6695bec748b2e1cdd45967a76553b662ca8138a3
Iqbal Virdaus s.s <doly13pharjalang@gmail.com>
2017-05-05 13:28:36 +0700

    5/5/2017 : 1%

--- src/ProjectAkhir/koneksi.java
+++ src/ProjectAkhir/koneksi.java
@@ -16,7 +16,7 @@ public class koneksi {
 private static Connection con;
     public static Connection getConnection(){
         try{
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projek_visual","root","");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tiket_travel","root","");
             JOptionPane.showMessageDialog(null,"Koneksi Berhasil");
         }catch(SQLException e){
             JOptionPane.showMessageDialog(null,"Koneksi Gagal: "+e.getMessage());
